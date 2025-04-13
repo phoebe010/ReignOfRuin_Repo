200f7c84b34241ccf1cfb4e0a0fd9a860cf18fbd
et402 <ethee.tran@gmail.com>
2025-04-13 14:35:07 -0400

    Grid System and Dialogue System

--- ReignOfRuin/Assets/Scripts/Dialogue_System/DialogueHandler.cs
+++ ReignOfRuin/Assets/Scripts/Dialogue_System/DialogueHandler.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueHandler : MonoBehaviour
 {
+    public static DialogueHandler _Instance;
+
     public TestDialogue testDialogue; 
+    public TextMeshProUGUI txtToScreen;
+    public float delay = 0.1f;
+
+    private void Awake()
+    {
+        if (null == _Instance)
+            _Instance = this;
+        else Destroy(gameObject);
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        testDialogue.inx = 0;
-        Debug.Log(testDialogue.dialogueSequence[testDialogue.inx]);         
+        testDialogue.inx = 0;        
+        StartCoroutine(TypeWriter(testDialogue.dialogueSequence[testDialogue.inx]));
     }
 
     // Update is called once per frame
@@ -20,8 +34,15 @@ public class DialogueHandler : MonoBehaviour
             
             if (testDialogue.inx >= testDialogue.dialogueSequence.Capacity)
                 return;
-            else 
-                Debug.Log(testDialogue.dialogueSequence[testDialogue.inx]);
+            else  
+                StartCoroutine(TypeWriter(testDialogue.dialogueSequence[testDialogue.inx]));
+        }
+    }
+
+    IEnumerator TypeWriter(string dialogue) {
+        for (int i=0; i<testDialogue.dialogueSequence[testDialogue.inx].Length+1; i++) {
+            txtToScreen.text = testDialogue.dialogueSequence[testDialogue.inx].Substring(0, i);
+            yield return new WaitForSeconds(delay);
         }
     }
 }
---
+++ ReignOfRuin/Assets/Scripts/Grid_System/GridManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] Vector2Int gridSize;
+    [SerializeField] int unityGridSize; 
+    public int UnityGridSize {get {return unityGridSize;} }
+
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+    Dictionary<Vector2Int, Node> Grid {get {return grid;} }
+
+    private void Awake()
+    {
+        for(int x=0; x<gridSize.x; x++) {
+            for (int y=0; y<gridSize.y; y++) {
+                Vector2Int cords = new Vector2Int(x, y);
+                grid.Add(cords, new Node(cords));
+            }
+        }
+    }
+}

bcab9358c02884f67e1601e03b2ee7db9eeefb6b
et402 <ethee.tran@gmail.com>
2025-04-17 15:18:37 -0400

    Improvements on troop scripts

--- ReignOfRuin/Assets/Scripts/Unit_System/States/TroopOpponent.cs
+++ ReignOfRuin/Assets/Scripts/Unit_System/States/TroopOpponent.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Troop : MonoBehaviour, UnitInterface 
+public class TroopOpponent : MonoBehaviour, UnitInterface 
 {
     //turn this into two scripts
     public TroopStats troopStats;  
@@ -11,14 +11,12 @@ public class Troop : MonoBehaviour, UnitInterface
     private Vector2Int testCords = new Vector2Int(0, 0); 
 
     private void Awake()
-    {   
-        transform.parent.position = new Vector3(tC.teleCords.x, transform.parent.position.y, tC.teleCords.y); 
-        
+    {  
         troopStats.xPosition = Mathf.RoundToInt(transform.parent.position.x);
         troopStats.yPosition = Mathf.RoundToInt(transform.parent.position.z); 
 
         Debug.Log(GridManager._Instance);
- 
+
         StartCoroutine(WaitForInstance());
     } 
 
@@ -28,20 +26,20 @@ public class Troop : MonoBehaviour, UnitInterface
             yield return null;
         Debug.Log("GridManager ready");
 
-        StartCoroutine(MoveOnGrid());
+        StartCoroutine(MoveOnGridOpponent());
     }
-
-    private IEnumerator MoveOnGrid()
-    {
-        if (transform.parent.position.x < GridManager._Instance.grid[troopStats.TargCordCompiler()].cords.x) {
-            for (; transform.parent.position.x < GridManager._Instance.grid[troopStats.TargCordCompiler()].cords.x; 
+ 
+    private IEnumerator MoveOnGridOpponent()
+    { 
+        if (transform.parent.position.x < GridManager._Instance.grid[testCords].cords.x) {
+            for (; transform.parent.position.x < GridManager._Instance.grid[testCords].cords.x; 
                 transform.parent.position = new Vector3(transform.parent.position.x+1, transform.parent.position.y, transform.parent.position.z)) {
                 if (transform.parent.position.x  == 0) continue;
                 
                 yield return new WaitForSeconds(troopStats.speed);
             }
-        } else if (transform.parent.position.x > GridManager._Instance.grid[troopStats.TargCordCompiler()].cords.x) {
-            for (; transform.parent.position.x > GridManager._Instance.grid[troopStats.TargCordCompiler()].cords.x; 
+        } else if (transform.parent.position.x > GridManager._Instance.grid[testCords].cords.x) {
+            for (; transform.parent.position.x > GridManager._Instance.grid[testCords].cords.x; 
                 transform.parent.position = new Vector3(transform.parent.position.x-1, transform.parent.position.y, transform.parent.position.z)) {
                 if (transform.parent.position.x  == 0) continue;
                 
@@ -49,14 +47,15 @@ public class Troop : MonoBehaviour, UnitInterface
             }
         }
 
-        for (; transform.parent.position.z < GridManager._Instance.grid[troopStats.TargCordCompiler()].cords.y; 
-            transform.parent.position = new Vector3(transform.parent.position.x, transform.parent.position.y, transform.parent.position.z+1)) { 
+        for (; transform.parent.position.z > GridManager._Instance.grid[testCords].cords.y; 
+            transform.parent.position = new Vector3(transform.parent.position.x, transform.parent.position.y, transform.parent.position.z-1)) { 
+            //Debug.Log(GridManager._Instance.grid[troopStats.TargCordCompiler()].cords.y);
             if (transform.parent.position.z == 0) continue;
 
             yield return new WaitForSeconds(troopStats.speed);
         }
     }
-    
+
     private void OnTriggerEnter(Collider other)
     {
 

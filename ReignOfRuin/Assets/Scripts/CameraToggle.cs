ad38a4957b4119f0c4dfe8f5c17bdd9bec425d39
ArielHalpert <105675762+ArielHalpert@users.noreply.github.com>
2025-04-30 22:27:51 -0400

    Coin Stuff

--- ReignOfRuin/Assets/Scripts/CameraToggle.cs
+++ ReignOfRuin/Assets/Scripts/CameraToggle.cs
@@ -5,22 +5,37 @@ public class CameraToggle : MonoBehaviour
     private Camera Camera1;
     private Camera Camera2;
 
-    public int Manager;
+    public int Manager = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public void Start(){
         Camera1 = GameObject.Find ("Main Camera").GetComponent<Camera> ();
         Camera2 = GameObject.Find ("Lanes Camera").GetComponent<Camera> ();
 
+<<<<<<< Updated upstream
         if (Manager == 0){
             Camera1.rect = new Rect(0, 0, .6f, 1);
+=======
+        
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.C)){
+            CameraChange();
+        }
+    }
+
+    void CameraChange(){
+        if (Manager == 1){
+>>>>>>> Stashed changes
             Camera1.rect = new Rect(0, 0, 1, .6f);
-            Manager = 1;
+            Camera2.rect = new Rect(0, .6f, 1, 1);
+            Manager = 0;
         }
         else{
             Camera1.rect = new Rect(0, 0, 1, 1);
-            Camera1.rect = new Rect(0, 0, 0, 0);
-            Manager = 0;
+            Camera2.rect = new Rect(0, 0, 0, 0);
+            Manager = 1;
         }
     }
 }

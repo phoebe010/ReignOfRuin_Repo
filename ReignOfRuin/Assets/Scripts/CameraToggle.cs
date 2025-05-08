40ac21fd57950c3038b0f25cf12ec70704c41f6d
ArielHalpert <105675762+ArielHalpert@users.noreply.github.com>
2025-05-08 13:22:47 -0400

    Vertical Camera Plus Toggle
    Change left and right side of lanes

--- ReignOfRuin/Assets/Scripts/CameraToggle.cs
+++ ReignOfRuin/Assets/Scripts/CameraToggle.cs
@@ -5,7 +5,8 @@ public class CameraToggle : MonoBehaviour
     private Camera Camera1;
     private Camera Camera2;
 
-    public int Manager = 0;
+    public int CameraManager = 0;
+    public int LaneManager = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public void Start(){
@@ -16,20 +17,36 @@ public class CameraToggle : MonoBehaviour
 
     void Update(){
         if(Input.GetKeyDown(KeyCode.C)){
-            CameraChange();
+            LaneOn();
+        }
+        if(Input.GetKeyDown(KeyCode.V)){
+            LaneToggle();
         }
     }
 
-    void CameraChange(){
-        if (Manager == 1){
-            Camera1.rect = new Rect(0, 0, 1, .6f);
-            Camera2.rect = new Rect(0, .6f, 1, 1);
-            Manager = 0;
+    void LaneOn(){
+        if (CameraManager == 1){
+            Camera1.rect = new Rect(0, 0, .8f, 1);
+            Camera2.rect = new Rect(.8f, 0, 1, 1);
+            CameraManager = 0;
         }
         else{
             Camera1.rect = new Rect(0, 0, 1, 1);
             Camera2.rect = new Rect(0, 0, 0, 0);
-            Manager = 1;
+            CameraManager = 1;
+        }
+    }
+
+    void LaneToggle(){
+        if (LaneManager == 1){
+            Camera1.rect = new Rect(0, 0, .8f, 1);
+            Camera2.rect = new Rect(.8f, 0, 1, 1);
+            LaneManager = 0;
+        }
+        else{
+            Camera1.rect = new Rect(.2f, 0, 1, 1);
+            Camera2.rect = new Rect(0, 0, .2f, 1);
+            LaneManager = 1;
         }
     }
 }
